8a23e565847b0ca9d32994341f525135901c8f76
utsavkhan <38136166+utsavkhan@users.noreply.github.com>
2018-04-25 12:54:26 +0200

    updated

--- Tasks.m
+++ Tasks.m
@@ -20,7 +20,7 @@ new_SWA_VBOX = [Time SWA_VBOX];
 %% washout filter-based
 
 
-T=0.47;
+T=3.25;
 sim('TasksSimulink1a')
 plot(Time,beta_washout.Data,'-.r')
 hold on
@@ -38,7 +38,7 @@ hold on;
 % %---------------------------------------------------------
 % CALCULATE THE ERROR VALES FOR THE ESTIMATE OF SLIP ANGLE
 %--------------------------------------------------------- 
-[e_beta_mean,e_beta_max,time_at_max,error] = errorCalc(beta_modelbased.Data,Beta_VBOX);
+[e_beta_mean,e_beta_max,time_at_max,error] = errorCalc(beta_integration.Data,Beta_VBOX);
 disp(' ');
 fprintf('The MSE of Beta estimation is: %d \n',e_beta_mean);
 fprintf('The Max error of Beta estimation is: %d \n',e_beta_max);
