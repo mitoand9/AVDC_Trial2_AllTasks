7c8065dc74654b8a8f1b9ecc1f612084bb6b9293
mitoand9 <mitoand9@yahoo.it>
2018-05-01 10:44:10 +0200

    kk

--- Vehicle_measure_eq.m
+++ Vehicle_measure_eq.m
@@ -5,5 +5,12 @@ function y_n = Vehicle_measure_eq(x,param)
 
 global lf lr mass Cf Cr
 
+alpha_12 = atan((x(2,:) + x(3,:)*lf)/x(1,:))-param.delta;
+alpha_34 = atan((x(2,:) - x(3,:)*lr)/x(1,:));
 
-y_n =
+F_12 = -C12*alpha_12;
+F_34 = -C34*alpha_34;
+
+ay = (1/mass)*(F34 + F12*cos(param.delta));
+
+y_n = [x(1,:) ay x(3,:)];
--- Vehicle_state_eq.m
+++ Vehicle_state_eq.m
@@ -5,11 +5,11 @@ function x_n = Vehicle_state_eq(x,param)
 
 global lf lr mass Iz Cf Cr
 
-alpha_12 = ((x(2)+x(4)*f)/velocity)-delta_t;
-alpha_34 = ((x(2)-x(4)*b)/velocity);
+alpha_12 = atan((x(2,:) + x(3,:)*lf)/x(1,:))-param.delta;
+alpha_34 = atan((x(2,:) - x(3,:)*lr)/x(1,:));
 
-F_12 = -C12*tan(alpha_12);
-F_34 = -C34*tan(alpha_34);
+F_12 = -C12*alpha_12;
+F_34 = -C34*alpha_34;
 
 vxdot = (1/mass)*(-F12*sin(param.delta)) + x(2,:)*x(3,:);
 vydot = (1/mass)*(F34 + F12*cos(param.delta) + x(1,:)*x(3,:));
