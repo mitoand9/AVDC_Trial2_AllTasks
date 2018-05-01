7f95252027b134943a2d26435735be7937f4e1f6
mitoand9 <mitoand9@yahoo.it>
2018-05-01 10:38:07 +0200

    io

--- Vehicle_state_eq.m
+++ Vehicle_state_eq.m
@@ -5,11 +5,17 @@ function x_n = Vehicle_state_eq(x,param)
 
 global lf lr mass Iz Cf Cr
 
-vxdot = psidot*vy + (Cf*vy*delta)/(vx*mass) + (Cf*psidot*lf*delta)/(vx*mass) - Cf*delta^2; %vxdot
-vydot = -lf*Cf*(vy + psidot*lf)/vx + lr*Cr %vydot
-psidotdot = %psidotdot
+alpha_12 = ((x(2)+x(4)*f)/velocity)-delta_t;
+alpha_34 = ((x(2)-x(4)*b)/velocity);
 
-f_x = (x1,x2,x3);
+F_12 = -C12*tan(alpha_12);
+F_34 = -C34*tan(alpha_34);
+
+vxdot = (1/mass)*(-F12*sin(param.delta)) + x(2,:)*x(3,:);
+vydot = (1/mass)*(F34 + F12*cos(param.delta) + x(1,:)*x(3,:));
+psidotdot = (1/Iz)*(lf*F12*cos(param.delta)-lr*F34);
+
+f_x = [vxdot vydot psidotdot];
 
 % Integrate using Runge Kutta (in the script folder) or simple euler forward
 
