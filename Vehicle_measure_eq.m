42b715fe01bd8ceff0a2cb951e44728a1986225c
mitoand9 <mitoand9@yahoo.it>
2018-05-02 10:00:19 +0200

    jjkk

--- Vehicle_measure_eq.m
+++ Vehicle_measure_eq.m
@@ -3,14 +3,14 @@ function y_n = Vehicle_measure_eq(x,param)
 %    x = the states
 %    param = parameters that you might need, such as vehicle parameters.
 
-global lf lr mass Cf Cr
+global lf lr mass Cf Cr deltatrial
 
-alpha_12 = atan((x(2,:) + x(3,:)*lf)/x(1,:))-param.delta;
-alpha_34 = atan((x(2,:) - x(3,:)*lr)/x(1,:));
+alpha12 = atan((x(2,:) + x(3,:)*lf)/x(1,:))-deltatrial;
+alpha34 = atan((x(2,:) - x(3,:)*lr)/x(1,:));
 
-F_12 = -C12*alpha_12;
-F_34 = -C34*alpha_34;
+F12 = -Cf*alpha12;
+F34 = -Cr*alpha34;
 
-ay = (1/mass)*(F34 + F12*cos(param.delta));
+ay = (1/mass)*(F34 + F12*cos(deltatrial));
 
 y_n = [x(1,:) ay x(3,:)];
