411eb9719a064c7fc49b99a5110abedca54053a0
mitoand9 <mitoand9@yahoo.it>
2018-05-06 14:43:29 +0200

    asdjsh

--- Vehicle_state_eq.m
+++ Vehicle_state_eq.m
@@ -12,7 +12,7 @@ F12 = -Cf*alpha12;
 F34 = -Cr*alpha34;
 
 vxdot = (1/mass)*(-F12*sin(deltatrial)) + x(2,:).*x(3,:);
-vydot = (1/mass)*(F34 + F12*cos(deltatrial) + x(1,:).*x(3,:));
+vydot = (1/mass)*(F34 + F12*cos(deltatrial)) + x(1,:).*x(3,:);
 psidotdot = (1/Iz)*(lf*F12*cos(deltatrial)-lr*F34);
 
 f_x = [vxdot vydot psidotdot]';
